69699efb83d242fd7190cf26fcddea4c5b4885d2
Jonathan Webb <jonathan@webbdev.ca>
2015-12-24 21:37:11 -0500

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    require("transform-es2015-parameters"),
+    require("transform-es2015-unicode-regex"),
+    require("transform-es2015-sticky-regex"),
+    require("transform-es2015-block-scoping"),
+    require("transform-es2015-classes"),
+    require("transform-es2015-object-super"),
+    require("transform-es2015-function-name"),
+  ]
+};
