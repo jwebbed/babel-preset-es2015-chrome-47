d2f3b72f5e28265cd5d871c76013e60993d5bf53
Jonathan Webb <jonathan@webbdev.ca>
2015-12-24 21:54:04 -0500

    forgot about destructuring

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ module.exports = {
     require("babel-plugin-transform-es2015-object-super"),
     require("babel-plugin-transform-es2015-function-name"),
     require("babel-plugin-transform-es2015-modules-commonjs"),
-    require("babel-plugin-transform-es2015-arrow-functions")
+    require("babel-plugin-transform-es2015-arrow-functions"),
+    require("babel-plugin-transform-es2015-destructuring")
   ]
 };
