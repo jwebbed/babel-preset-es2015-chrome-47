ab0cf555ca17efba3736abb219be9dc68babd6cf
Jonathan Webb <jonathan@webbdev.ca>
2015-12-24 21:48:05 -0500

    forgot commonjs

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ module.exports = {
     require("babel-plugin-transform-es2015-classes"),
     require("babel-plugin-transform-es2015-object-super"),
     require("babel-plugin-transform-es2015-function-name"),
+    require("babel-plugin-transform-es2015-modules-commonjs")
   ]
 };
