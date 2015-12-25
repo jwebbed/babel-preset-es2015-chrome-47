f8e3344a65450d03dd1b057ef925a4e3f056d340
Jonathan Webb <jonathan@webbdev.ca>
2015-12-24 21:44:41 -0500

    pls gib diretide

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
   plugins: [
-    require("transform-es2015-parameters"),
-    require("transform-es2015-unicode-regex"),
-    require("transform-es2015-sticky-regex"),
-    require("transform-es2015-block-scoping"),
-    require("transform-es2015-classes"),
-    require("transform-es2015-object-super"),
-    require("transform-es2015-function-name"),
+    require("babel-plugin-transform-es2015-parameters"),
+    require("babel-plugin-transform-es2015-unicode-regex"),
+    require("babel-plugin-transform-es2015-sticky-regex"),
+    require("babel-plugin-transform-es2015-block-scoping"),
+    require("babel-plugin-transform-es2015-classes"),
+    require("babel-plugin-transform-es2015-object-super"),
+    require("babel-plugin-transform-es2015-function-name"),
   ]
 };
